ae54d5515c4c1b90c1ebae1c36674fca060dcbc2
VictorMtsx <joaovictormts.087@gmail.com>
2025-01-13 18:32:26 -0300

    estabelecendo conexão com o postgres e criando as tabelas

---
+++ server/migrations/0000_lumpy_klaw.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "users" (
+	"id" varchar(10) PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"nickname" text NOT NULL,
+	"password" varchar(20) NOT NULL
+);
---
+++ server/src/db/schema.ts
@@ -0,0 +1,14 @@
+import { pgTable, text, integer, varchar } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
+
+export const users = pgTable("users", {
+	id: text("id")
+		.primaryKey()
+		.$default(() => createId()),
+	fullName: text("name").notNull(),
+	email: varchar("email", { length: 50 }).notNull(),
+	nickname: text("nickname")
+		.notNull()
+		.$default(() => "Sem apelido"),
+	password: varchar("password", { length: 20 }).notNull(),
+});
